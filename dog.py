62eaab58ffc9f4d72619ad132f38428b6283c9e1
jowe9773 <jowe9773@colorado.edu>
2024-02-27 14:14:05 -0700

    add description and speak methods

--- dog.py
+++ dog.py
@@ -1,20 +1,28 @@
 # dog.py
 
-class Dog:                              # create a class about dogs
-    species = "Canis familiaris"        # class attributes - something that is the same for every instance of a class
+class Dog:                                                  # create a class about dogs
+    species = "Canis familiaris"                            # class attributes - something that is the same for every instance of a class
 
-    def __init__(self, name, age):      # Every time you make a new instance of the Dog class, this method will run and it sets the initial state of the object. Parameters that every instance of a class will have should be defined in this method
+    def __init__(self, name, age):                          # Every time you make a new instance of the Dog class, this method will run and 
+                                                            #   it sets the initial state of the object. Parameters that every instance of a
+                                                            #   class will have should be defined in this method
         
-        self.name = name                # Creates an attribute called name and assigns it the value of the variable name 
-        self.age = age                  # These are instance attributes
+        self.name = name                                    # Creates an attribute called name and assigns it the value of the variable name 
+        self.age = age                                      # These are instance attributes
+
+    def description(self):                                  # Method to describe the instance of the class
+        return f"{self.name} is {self.age} years old"
+
+    def speak(self, sound):                                 # Method to have the class instance do something ("speak")
+        return f"{self.name} says {sound}!"
 
     
 
-fido = Dog("Fido", 15)                         #instantiate the dog class for fido
+fido = Dog("Fido", 15)                                      #instantiate the dog class for fido
 
-print(fido.name)                               #print the instance and class attributes using 'dot notation'
+print(fido.name)                                            #print the instance and class attributes using 'dot notation'
 print(fido.age)
 print(fido.species)
 
-fido.species = "Felis silvestris"              #can change attributes (both class and instance) for a particular instance
+fido.species = "Felis silvestris"                           #can change attributes (both class and instance) for a particular instance
 print(fido.species)
