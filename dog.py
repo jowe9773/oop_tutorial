cf467a91cf7097abdef6877b74835c5fd3e5d2d0
jowe9773 <jowe9773@colorado.edu>
2024-02-27 13:54:38 -0700

    adding __init__ method

--- dog.py
+++ dog.py
@@ -1,4 +1,6 @@
 # dog.py
 
-class Dog:
-    pass
+class Dog:                              # create a class about dogs
+    def __init__(self, name, age):      # Every time you make a new instance of the Dog class, this method will run and it sets the initial state of the object. Parameters that every instance of a class will have should be defined in this method
+        self.name = name
+        self.age = age
